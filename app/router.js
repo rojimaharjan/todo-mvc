6231a908e2cd61f53570323fb2194792a57aef06
roji-mits <roji@makeitsimple.info>
2016-01-12 16:14:21 +0545

    first todo project without PODS

--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('todos', function() {
+    this.route('incomplete');
+    this.route('complete');
+  });
 });
 
 export default Router;
